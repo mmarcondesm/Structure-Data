b1b8f13c73415863ad301aa00f8a25f65d68ba79
mmarcondesm <matheus.marcondes@outlook.com>
2025-10-28 23:25:35 -0300

    Introduxtion of DataStructure

--- MyFirstJavaApp.java
+++ MyFirstJavaApp.java
@@ -4,9 +4,29 @@ package Java;
 public class MyFirstJavaApp {
     
     public static void main(String[] args) {
+        int[] vetor = new int[10];
         
-        int idade = 19;
-        String name = "Luiz";
-        System.out.println("Idade do cidadão é: " + idade + " e seu nome é: " + name);
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random()* vetor.length);
+            System.out.println(vetor[i]);
+            }
+
+        int aux;
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[i] > vetor[j]) {
+                   aux = vetor[j];
+                   vetor[j] = vetor[i]; 
+                   vetor[i] = aux;
+                }
+            }
+        }
+
+        System.out.println("Vetor ordenado");
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println(vetor[i]);
+        }
+
+        }
     }
-}
+
